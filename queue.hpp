b6a8994c6a421429d327699edf8a6a3f90622c09
Ivan Ribeiro <ivan.ribeiro@gmail.com>
2010-01-13 10:19:26 -0200

    dequeue mutex (GET)

--- queue.hpp
+++ queue.hpp
@@ -11,18 +11,11 @@
 #ifndef HTTP_SERVER3_QUEUE_HPP
 #define HTTP_SERVER3_QUEUE_HPP
 
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <exception>
 #include <boost/noncopyable.hpp>
-#include "boost/lexical_cast.hpp"
+#include <boost/thread/mutex.hpp>
 #include "globals.hpp"
 #include "reply.hpp"
 #include "request.hpp"
-#include "request_handler.hpp"
-#include "soci.h"
-#include "soci-mysql.h"
 
 namespace http {
     namespace server3 {
@@ -32,122 +25,11 @@ namespace http {
             : private boost::noncopyable
         {
         public:
-            int operator() (const request& req, reply& rep)
-            {
-                try
-                {
-                    // Check for valid requests.
-                    std::string action((req.uri.size() > 1) ? req.uri.substr(1) : "");
-
-                    if ((!action.empty()) &&
-                        (action != "spy") && (action != "size") && (action != "count"))
-                    {
-                        if (req.method == "GET")
-                        {
-                            rep = reply::stock_reply(reply::bad_request);
-                            return request_handler::finished;
-                        }
-                    }
-
-                    // Check for queries size/count/spy or dequeue(default)
-                    if (req.method == "GET")
-                    {
-                        if (action == "size" || action == "count")
-                        {
-                            int count;
-                            (*req.sql) << "SELECT COUNT(*) FROM q", soci::into(count);
-
-                            std::stringstream scount;
-                            scount << count;
-
-                            rep.content = scount.str();
-
-                            content(req, rep);
-                            return request_handler::finished;
-                        }
-
-                        // Retrieve data and k
-                        // URI must be: /spy or / (dequeue)
-                        unsigned long k;
-                        std::string d;
-
-                        (*req.sql).begin();
-
-                        (*req.sql) << "SELECT k, d FROM q ORDER BY p DESC,k", soci::into(k), soci::into(d);
-
-                        rep.content = d;
-
-                        // Dequeue item if URI = "/"
-                        if (action.empty())
-                        {
-                            soci::statement st = ((*req.sql).prepare << "DELETE FROM q where k = :k",
-                                                  soci::use(k));
-
-                            st.execute(true);
-                        }
-
-                        (*req.sql).commit();
-
-                        // Check for 404 (empty/no data) or 200
-                        if (d.empty())
-                            rep = reply::stock_reply(reply::not_found);
-                        else
-                            content(req, rep);
-
-                        return request_handler::finished;
-                    }
-                    else if (req.method == "POST")
-                    {
-                        if (req.post_data.size() > 2)
-                        {
-                            std::string uri((req.uri.size() > 1) ? req.uri.substr(1) : "0");
-
-                            std::string d = req.post_data.substr(2);
-
-                            int p = boost::lexical_cast<int>(uri);
-
-                            soci::statement st = ((*req.sql).prepare << "INSERT INTO q(d, p) VALUES (:d, :p)",
-                                                  soci::use(d),
-                                                  soci::use(p));
-
-                            st.execute(true);
-
-                            content(req, rep);
-                        }
-                        else
-                        {
-                            rep = reply::stock_reply(reply::bad_request);
-                        }
-
-                        return request_handler::finished;
-                    }
-                }
-                catch (std::exception const &e)
-                {
-                    rep = reply::stock_reply(reply::internal_server_error);
-
-                    LIERR(e.what());
-
-                    return request_handler::finished;
-                }
-
-                return request_handler::declined;
-            }
+            int operator() (const request& req, reply& rep);
         private:
-            void content(const request& req, reply& rep)
-            {
-                header hcl, hct;
+            void content(const request& req, reply& rep);
 
-                hcl.name = CONTENT_LENGTH;
-                hcl.value = boost::lexical_cast<std::string>(rep.content.size());
-                rep.headers.push_back(hcl);
-
-                hct.name = CONTENT_TYPE;
-                hct.value = MIME_TYPE;
-                rep.headers.push_back(hct);
-
-                rep.status = reply::ok;
-            }
+			static boost::mutex dequeue_mutex_;
         };
 
     } // namespace server3
